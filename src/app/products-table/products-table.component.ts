42d31a3e4b307968c956dc8b88dde90b63a12a63
xdguido <guidogennari95@gmail.com>
2023-07-16 18:09:46 -0300

    add show/hide button

--- src/app/products-table/products-table.component.ts
+++ src/app/products-table/products-table.component.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
 
 type Product = {
     id: number;
@@ -12,13 +13,18 @@ type Product = {
     templateUrl: './products-table.component.html',
     styleUrls: ['./products-table.component.css'],
     standalone: true,
-    imports: [CommonModule]
+    imports: [CommonModule, MatButtonModule]
 })
 export class ProductsTableComponent {
-    columnsToDisplay: string[] = ['id', 'Product Name', 'Product Price'];
+    show: boolean = false;
+    columnsToDisplay: string[] = ['Name', 'Price'];
     productsList: Product[] = [
         { id: 1, name: 'Apple', price: 100 },
         { id: 2, name: 'Banana', price: 200 },
         { id: 3, name: 'Orange', price: 300 }
     ];
+
+    toggleShow() {
+        this.show = !this.show;
+    }
 }
